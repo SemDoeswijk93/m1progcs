8a61bfb020b8902df2d81d15a0502f9efcc2b61b
Sem <40010@ma-web.nl>
2025-11-06 10:31:46 +0100

    m1prog initial commit

---
+++ opdrachten/03_rekenen/enterplus/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace enterplus;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int enters = 0;
+        while(true)
+        {
+            enters++;
+            Console.WriteLine($"aantal enters is {enters}");
+            Console.ReadLine();
+
+        }
+    }
+}
--- opdrachten/03_rekenen/operators/Program.cs
+++ opdrachten/03_rekenen/operators/Program.cs
@@ -8,11 +8,40 @@ class Program
         int plus = 9;
         plus++;
 
-        Console.WriteLine(plus);
+        Console.WriteLine($"Ik heb er 1 bij gedaan want de waarde is nu {plus}");
 
         int min = 9;
         min--;
+        min--;
 
-        Console.WriteLine(min)
+        Console.WriteLine($"Ik heb er 2 afgehaald want de waarde is nu {min}");
+
+        int voorbeeld1 = 20;
+        voorbeeld1 += 10;
+        Console.WriteLine($"Voorbeeld 1 is nu 30, kijk {voorbeeld1}");
+
+        int voorbeeld2 = 20;
+        int result = voorbeeld2 + 10;
+        Console.WriteLine($"Voorbeeld 2 blijft 20 want kijk {voorbeeld2} terwijl result wel {result} is.");
+
+        int voorbeeld3 = 20;
+        int result2 = voorbeeld3 += 10;
+        Console.WriteLine($"Voorbeeld 3 is nu wel {voorbeeld3} net zo als result 2 die ook {result2} is. Dit komt door de += die we erbij hebben gedaan.");
+
+        //Zelf Opdrachten hieronder
+
+        int getalint = 163;
+        getalint -= 67;
+        Console.WriteLine($"De getal met de int wat eerst 163 was heeft nu een waarde van {getalint}");
+
+        double getaldouble = 67;
+        getaldouble *= 3;
+        Console.WriteLine($"De getal met de double wat eerst 67 was heeft nu een waarde van {getaldouble}");
+
+        float getalfloat = 690;
+        getalfloat /= 5;
+        Console.WriteLine($"De getal met de float wat eerst 690 was heeft nu een waarde van {getalfloat}");
+
+
     }
 }
